4784045de48a0a06935035424906fbcadde5c5f6
offlinenow <offlinenow@users.noreply.github.com>
2017-09-08 13:24:48 +0800

    对话框

--- app/src/main/java/com/example/myapplication/DialogBox.java
+++ app/src/main/java/com/example/myapplication/DialogBox.java
@@ -1,28 +1,76 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class DialogBox extends DialogFragment {
+    private ViewPager pager;
+    private Bundle savedInstanceState;
+    private Activity activities;
 
     @Override
-    public Dialog onCreateDialog(final Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.fragment_dialog_box, null))
-                .setPositiveButton(R.string.login, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
+    public void onAttach(Context context){
+        super.onAttach(context);
+        if (getActivity()==null){
+            this.activities=super.getActivity();
+        }
 
-                    }
-                });
-        return builder.create();
+
     }
+
+
+//    @Override
+//    @NonNull
+//    public Dialog onCreateDialog(Bundle savedInstanceState) {
+//        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+//        LayoutInflater inflater = LayoutInflater.from(getActivity());
+//        View viewroot = inflater.inflate(R.layout.fragment_dialog_box, null);
+//
+//        alertDialog
+//                .setIcon(R.drawable.ic_launcher_background)
+//                .setTitle(R.string.PleaseInputUserNameAndPassword)
+//                .setPositiveButton(R.string.login,
+//                        new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                final String username = getDialog().findViewById(R.id.username).toString();
+//                                final String password = ((EditText) getDialog().findViewById(R.id.password)).getText().toString();
+//                                if (!username.equals("abc") || !password.equals("123")) {
+//                                    CharSequence text = "登录失败";
+//                                    Toast toast = Toast.makeText(getActivity().findViewById(R.id.button1).getContext(), text, Toast.LENGTH_SHORT);
+//                                    toast.setGravity(Gravity.END | Gravity.CENTER, 0, 0);
+//                                    toast.show();
+//                                } else {
+//                                    CharSequence text = "登录成功";
+//                                    Toast toast = Toast.makeText(getActivity().findViewById(R.id.button1).getContext(), text, Toast.LENGTH_SHORT);
+//                                    toast.setGravity(Gravity.END | Gravity.CENTER, 0, 0);
+//                                    toast.show();
+//                                }
+//
+//                            }
+//                        })
+//                .setNegativeButton(R.string.cancel,
+//                        new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                DialogBox.this.getDialog().cancel();
+//                            }
+//                        });
+//        alertDialog.setView(viewroot);
+//        return alertDialog.create();
+//    }
 }
 
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,23 +1,73 @@
 package com.example.myapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void login(View view) {
+//        DialogFragment dialogFragment = new DialogFragment();
+//        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+//        dialogFragment.show(fragmentTransaction,"dialog");
 
+//        DialogBox dialogBox = new DialogBox();
+//        dialogBox.onCreateDialog(Bundle.EMPTY);
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+        final View viewroot = inflater.inflate(R.layout.fragment_dialog_box, null);
+
+        alertDialog
+                .setIcon(R.drawable.ic_launcher_background)
+                .setTitle(R.string.PleaseInputUserNameAndPassword)
+                .setPositiveButton(R.string.login,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                String username = ((EditText) viewroot.findViewById(R.id.username)).getText().toString();
+                                String password = ((EditText) viewroot.findViewById(R.id.password)).getText().toString();
+                                if (!username.equals("abc") || !password.equals("123")) {
+                                    CharSequence text = "登录失败";
+                                    Toast toast = Toast.makeText(MainActivity.this.getApplicationContext(), text, Toast.LENGTH_SHORT);
+                                    toast.setGravity(Gravity.END | Gravity.CENTER, 0, 0);
+                                    toast.show();
+                                } else {
+                                    CharSequence text = "登录成功";
+                                    Toast toast = Toast.makeText(MainActivity.this.getApplicationContext(), text, Toast.LENGTH_SHORT);
+                                    toast.setGravity(Gravity.END | Gravity.CENTER, 0, 0);
+                                    toast.show();
+                                }
+
+                            }
+                        })
+                .setNegativeButton(R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        });
+        alertDialog.setView(viewroot);
+        alertDialog.show();
     }
 
-    public void PopUpWindow(View view) {
+    public void popUpWindow(View view) {
 
     }
 
